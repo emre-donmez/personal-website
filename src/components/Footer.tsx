81f7f3a8932c452a66f3800a485060c92a6d80ea
Emrecan <emrecandonmez.bt@gmail.com>
2025-03-08 20:35:06 +0300

    Footer text has been upgraded

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="py-8 px-4 bg-[rgb(var(--color-bg))] border-t border-[rgb(var(--color-bg-secondary))]/30">
       <div className="max-w-5xl mx-auto text-center">
         <p className="text-[rgb(var(--color-text-secondary))/50] text-xs">
-          &copy; {new Date().getFullYear()} Emre Dönmez. All rights reserved.
+          &copy; {new Date().getFullYear()} Emrecan Dönmez. All rights reserved.
         </p>
       </div>
     </footer>
